cda434cb8877d0e939cc2524909defe1e4361bc6
Bruno Rocha Moura <brunorochamoura@proton.me>
2023-12-15 20:33:52 +0000

    Instead of looking for suid binaries, receives as argument a text file with the suid search output

--- suider.sh
+++ suider.sh
@@ -5,9 +5,22 @@ echo "==================================="
 echo " SUIDER - SUID Exploit Finder Tool "
 echo "==================================="
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 /path/to/suid_output.txt"
+    exit 1
+fi
+
+suid_file="$1"
+
+if [ ! -f "$suid_file" ]; then
+    echo "Error: File $suid_file not found."
+    exit 1
+fi
+
 echo "[+] Looking for standard exploitable SUID binaries...."
 
-suid_binaries=$(find / -perm -u=s -type f 2>/dev/null | rev | cut -d'/' -f 1 | rev)
+# Read the list of SUID binaries from the specified text file
+suid_binaries=$(cat "$suid_file" | rev | cut -d'/' -f 1 | rev)
 
 suid=(
     aa-exec ab agetty alpine ar arj arp as ascii-xfr ash aspell atobm awk base32 base64 basenc basez bash bc bridge busybox bzip2 cabal capsh cat chmod choom chown chroot clamscan cmp column comm cp cpio cpulimit csh csplit csvtool cupsfilter curl cut dash date dd debugfs dialog diff dig distcc dmsetup docker dosbox ed efax elvish emacs env eqn espeak expand expect file find fish flock fmt fold gawk gcore gdb genie genisoimage gimp grep gtester gzip hd head hexdump highlight hping3 iconv install ionice ip ispell jjs join jq jrunscript julia ksh ksshell kubectl ld.so less logsave look lua make mawk minicom more mosquitto msgattrib msgcat msgconv msgfilter msgmerge msguniq multitime mv nasm nawk ncftp nft nice nl nm nmap node nohup od openssl openvpn pandoc paste perf perl pexec pg php pidstat pr ptx python rc readelf restic rev rlwrap rsync rtorrent run-parts rview rvim sash scanmem sed setarch setfacl setlock shuf soelim softlimit sort sqlite3 ss ssh-agent ssh-keygen ssh-keyscan sshpass start-stop-daemon stdbuf strace strings sysctl systemctl tac tail taskset tbl tclsh tee terraform tftp tic time timeout troff ul unexpand uniq unshare unsquashfs unzip update-alternatives uudecode uuencode vagrant view vigr vim vimdiff vipw w3m watch wc wget whiptail xargs xdotool xmodmap xmore xxd xz yash zsh zsoelim
